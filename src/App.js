7415e395ca1d79c39ca8018da527794770a26e71
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-26 05:49:35 +0000

    actualizo Juego

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+import Tablero from './components/Tablero';
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>hola</h1>
+      <Tablero />
+      
+    </div>
+  );
+}
+
+export default App;
